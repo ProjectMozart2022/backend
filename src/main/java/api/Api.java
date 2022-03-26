757da2248e22a86e345d370af499949bba31fed3
MichalGebel-TomTom <michal.gebel@tomtom.com>
2022-03-26 15:41:55 +0100

    add cors

--- src/main/java/api/Api.java
+++ src/main/java/api/Api.java
@@ -11,6 +11,27 @@ public class Api {
   private static final StudentApi studentApi = new StudentApi();
 
   public static void main(String[] args) {
+      options("/*",
+              (request, response) -> {
+
+                  String accessControlRequestHeaders = request
+                          .headers("Access-Control-Request-Headers");
+                  if (accessControlRequestHeaders != null) {
+                      response.header("Access-Control-Allow-Headers",
+                              accessControlRequestHeaders);
+                  }
+
+                  String accessControlRequestMethod = request
+                          .headers("Access-Control-Request-Method");
+                  if (accessControlRequestMethod != null) {
+                      response.header("Access-Control-Allow-Methods",
+                              accessControlRequestMethod);
+                  }
+
+                  return "OK";
+              });
+
+      before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
     path(
         "/api",
         () -> {
@@ -41,3 +62,4 @@ public class Api {
         });
   }
 }
+
