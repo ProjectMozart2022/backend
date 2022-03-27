7255184452c1a6ed734802c5baf2283ed0e9537a
MichalGebel-TomTom <michal.gebel@tomtom.com>
2022-03-27 16:20:06 +0200

    preflight and cors

--- src/main/java/api/Api.java
+++ src/main/java/api/Api.java
@@ -11,8 +11,27 @@ public class Api {
     private static final StudentApi studentApi = new StudentApi();
 
     public static void main(String[] args) {
-        CorsFilter cors = new CorsFilter();
-        cors.apply();
+//      add preflight and cors
+        options("/*",
+                (request, response) -> {
+
+                    String accessControlRequestHeaders = request
+                            .headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+
+                    String accessControlRequestMethod = request
+                            .headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+
+                    return "OK";
+                });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
         path(
                 "/api",
                 () -> {
