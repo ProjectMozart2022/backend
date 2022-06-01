4eb2668c5f3e07170258e653c7beb23c782f0631
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-06-01 21:08:14 +0200

    unit tests

--- src/test/java/model/TestStudent.java
+++ src/test/java/model/TestStudent.java
@@ -6,22 +6,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class TestStudent {
-
+    private final Student emptyStudent = new Student();
     private final Student testStudent = new Student(1L, "Henryk", "Sienkiewicz", 2);
 
     @Test
     public void getStudentId() {
-        Student student = new Student();
-        assertEquals(student.getId(), 0);
+        assertEquals(emptyStudent.getId(), 0);
         assertEquals(testStudent.getId(), 1L);
     }
 
     @Test
     public void getFirstName() {
-        Student student = new Student();
-        assertNull(student.getFirstName());
+        assertNull(emptyStudent.getFirstName());
         assertEquals(testStudent.getFirstName(), "Henryk");
     }
-
-
 }
