72ca6a74f6046849664cd009c1d9cfe70be405f3
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-06-02 20:13:00 +0200

    not working yet

--- src/test/java/persistence/TestStudentPersistence.java
+++ src/test/java/persistence/TestStudentPersistence.java
@@ -1,25 +1,19 @@
 package persistence;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static util.DatabaseOps.performQuery;
+
 import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import javax.sql.DataSource;
-import org.jdbi.v3.core.Jdbi;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import model.Student;
+import org.junit.jupiter.api.*;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import static util.DatabaseOps.performQuery;
-
 public class TestStudentPersistence {
-  private static PostgreSQLContainer databaseContainer;
+  private static final PostgreSQLContainer databaseContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:alpine3.14")).;
   private static final HikariConfig config = new HikariConfig();
-  private static Jdbi jdbi = null;
   private static StudentPersistence studentPersistence = null;
 
   @BeforeAll
@@ -27,40 +21,32 @@ public class TestStudentPersistence {
     databaseContainer.start();
     final String port = databaseContainer.getFirstMappedPort().toString();
     String url = "jdbc:postgresql://localhost:" + port + "/postgres";
-    config.setJdbcUrl(url);
-    config.setUsername("sa");
-    config.setPassword("sa");
-    DataSource dataSource = new HikariDataSource(config);
-    jdbi = Jdbi.create(dataSource);
-    studentPersistence = new StudentPersistence();
   }
 
   @BeforeEach
   void initialize() throws Throwable {
-    databaseContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:alpine3.14"));
     databaseContainer.start();
     String parentPath = Path.of(System.getProperty("user.dir")).getParent().toString();
-    Path path = Path.of(parentPath + "/persistence/src/main/resources/database_creation.sql");
+    Path path = Path.of(parentPath + "/src/java/resources/queries/database_initialization.sql");
     performQuery(databaseContainer, Files.readString(path));
-    studentPersistence = new StudentPersistence(databaseContainer.getJdbcUrl(), databaseContainer.getUsername(), databaseContainer.getPassword(), 1);
-  }
-
-
-  @AfterAll
-  public static void tearDown() {
-    databaseContainer.stop();
+    studentPersistence =
+        new StudentPersistence(
+            databaseContainer.getJdbcUrl(),
+            databaseContainer.getUsername(),
+            databaseContainer.getPassword(),
+            1);
   }
 
   @AfterEach
-    public void tearDownEach() {
-        jdbi.useHandle(handle -> handle.execute("DELETE FROM student"));
-    }
+  public void tearDownEach() {
+    databaseContainer.stop();
+  }
 
-  //  @Test
-  //  public void testGetOne() {
-  //    Student student = studentPersistence.getOne(1);
-  //    assertEquals(student.getFirstName(), "Adam");
-  //    assertEquals(student.getLastName(), "kowalski");
-  //    assertEquals(student.getClassNumber(), 1);
-  //  }
+  @Test
+  public void testGetOne() {
+    Student student = studentPersistence.getOne(1);
+    assertEquals(student.getFirstName(), "Adam");
+    assertEquals(student.getLastName(), "kowalski");
+    assertEquals(student.getClassNumber(), 1);
+  }
 }
--- src/test/java/util/DatabaseOps.java
+++ src/test/java/util/DatabaseOps.java
@@ -2,33 +2,33 @@ package util;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.testcontainers.containers.JdbcDatabaseContainer;
-
-import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.sql.DataSource;
+import org.testcontainers.containers.JdbcDatabaseContainer;
 
 public class DatabaseOps {
 
-    public static ResultSet performQuery(JdbcDatabaseContainer<?> container, String sql) throws SQLException {
-        DataSource ds = getDataSource(container);
-        Statement statement = ds.getConnection().createStatement();
-        statement.execute(sql);
-        ResultSet resultSet = statement.getResultSet();
-        if (resultSet != null) {
-            resultSet.next();
-        }
-        return resultSet;
+  public static ResultSet performQuery(JdbcDatabaseContainer<?> container, String sql)
+      throws SQLException {
+    DataSource ds = getDataSource(container);
+    Statement statement = ds.getConnection().createStatement();
+    statement.execute(sql);
+    ResultSet resultSet = statement.getResultSet();
+    if (resultSet != null) {
+      resultSet.next();
     }
+    return resultSet;
+  }
 
-    private static DataSource getDataSource(JdbcDatabaseContainer<?> container) {
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(container.getJdbcUrl());
-        hikariConfig.setUsername(container.getUsername());
-        hikariConfig.setPassword(container.getPassword());
-        hikariConfig.setDriverClassName(container.getDriverClassName());
-        hikariConfig.setMaximumPoolSize(1);
-        return new HikariDataSource(hikariConfig);
-    }
+  private static DataSource getDataSource(JdbcDatabaseContainer<?> container) {
+    HikariConfig hikariConfig = new HikariConfig();
+    hikariConfig.setJdbcUrl(container.getJdbcUrl());
+    hikariConfig.setUsername(container.getUsername());
+    hikariConfig.setPassword(container.getPassword());
+    hikariConfig.setDriverClassName(container.getDriverClassName());
+    hikariConfig.setMaximumPoolSize(1);
+    return new HikariDataSource(hikariConfig);
+  }
 }
