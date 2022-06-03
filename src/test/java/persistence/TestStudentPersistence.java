3fe9d89f52133d6a8b5ef09eea7c55a8fdadd0a7
Piotr Tomczak <piotr.tomczak.praca@gmail.com>
2022-06-03 16:25:24 +0200

    persistance tests works finally

--- src/test/java/persistence/TestStudentPersistence.java
+++ src/test/java/persistence/TestStudentPersistence.java
@@ -3,31 +3,30 @@ package persistence;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static util.DatabaseOps.performQuery;
 
-import com.zaxxer.hikari.HikariConfig;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import model.Student;
 import org.junit.jupiter.api.*;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 public class TestStudentPersistence {
-  private static final PostgreSQLContainer databaseContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:alpine3.14")).;
-  private static final HikariConfig config = new HikariConfig();
-  private static StudentPersistence studentPersistence = null;
+  private static final PostgreSQLContainer databaseContainer =
+      new PostgreSQLContainer(DockerImageName.parse("postgres:alpine3.14"));
+  private StudentPersistence studentPersistence;
 
   @BeforeAll
   static void setUp() {
     databaseContainer.start();
-    final String port = databaseContainer.getFirstMappedPort().toString();
-    String url = "jdbc:postgresql://localhost:" + port + "/postgres";
   }
 
   @BeforeEach
   void initialize() throws Throwable {
     databaseContainer.start();
     String parentPath = Path.of(System.getProperty("user.dir")).getParent().toString();
-    Path path = Path.of(parentPath + "/src/java/resources/queries/database_initialization.sql");
+    Path path =
+        Path.of(parentPath + "/backend/src/main/resources/queries/database_initialization.sql");
     performQuery(databaseContainer, Files.readString(path));
     studentPersistence =
         new StudentPersistence(
@@ -35,6 +34,8 @@ public class TestStudentPersistence {
             databaseContainer.getUsername(),
             databaseContainer.getPassword(),
             1);
+    studentPersistence.add(new Student(1L, "Adam", "Kowalski", 1));
+    studentPersistence.add(new Student(2L, "Adam", "Niewiadomski", 1));
   }
 
   @AfterEach
@@ -43,10 +44,14 @@ public class TestStudentPersistence {
   }
 
   @Test
-  public void testGetOne() {
-    Student student = studentPersistence.getOne(1);
-    assertEquals(student.getFirstName(), "Adam");
-    assertEquals(student.getLastName(), "kowalski");
-    assertEquals(student.getClassNumber(), 1);
+  public void testGetAll() {
+    List<Student> students = studentPersistence.getAll();
+    assertEquals(students.size(), 2);
+    assertEquals(students.get(0).getFirstName(), "Adam");
+    assertEquals(students.get(0).getLastName(), "Kowalski");
+    assertEquals(students.get(0).getClassNumber(), 1);
+    assertEquals(students.get(1).getFirstName(), "Adam");
+    assertEquals(students.get(1).getLastName(), "Niewiadomski");
+    assertEquals(students.get(1).getClassNumber(), 1);
   }
 }
